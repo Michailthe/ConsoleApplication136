53fb5ffd834538480f369f9756b83e76f26c6c2c
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-16 01:43:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication136/ConsoleApplication136.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using  namespace std;
+int main() {
+    cout << "Это часы c кукушкой.\n";
+    cout << "Который час?\n";
+    int hour;
+    cin >> hour;
+    if (hour >= 0 and hour < 13)
+    {
+        int CooCoo;
+        if (hour == 0)
+        {
+            hour = 12;
+        }
+        while (CooCoo < hour) {
+            cout << "Coo-Coo" << endl;
+            CooCoo++;
+        }
+    }
+}
